1e9199c3b2b2f988eb0cbf94db3f58f4fa0cae65
Oidlichtnwoada <42490916+Oidlichtnwoada@users.noreply.github.com>
2024-01-24 22:58:38 +0100

    added cache to hold last value

--- blockchain_fee_logger/reporting/reporting.py
+++ blockchain_fee_logger/reporting/reporting.py
@@ -1,13 +1,50 @@
+from decimal import Decimal
+
 from blockchain_fee_logger.calculation.fee_calculation_result import (
     FeeCalculationResult,
 )
 from blockchain_fee_logger.logger.logger import LoggerFactory
 
 
+class FeeCache:
+    cache: dict[tuple[str, str], Decimal] = {}
+
+    @classmethod
+    def get_key_and_value_from_representation(
+        cls, result: FeeCalculationResult
+    ) -> tuple[tuple[str, str], Decimal]:
+        return (
+            (result.blockchain, result.unit),
+            result.fee,
+        )
+
+    @classmethod
+    def is_new_value(
+        cls, fee_calculation_result: FeeCalculationResult, update_cache: bool = False
+    ) -> bool:
+        key, new_value = cls.get_key_and_value_from_representation(
+            fee_calculation_result
+        )
+        if key not in cls.cache:
+            is_new = True
+        else:
+            current_value = cls.cache[key]
+            is_new = current_value != new_value
+        if update_cache:
+            cls.add_to_cache(fee_calculation_result)
+        return is_new
+
+    @classmethod
+    def add_to_cache(cls, fee_calculation_result: FeeCalculationResult) -> None:
+        key, value = cls.get_key_and_value_from_representation(fee_calculation_result)
+        cls.cache[key] = value
+
+
 def log_calculation_fee_result(result: FeeCalculationResult) -> None:
-    LoggerFactory.get_logger().info(
-        f"Fee for {result.blockchain} at {result.timestamp.isoformat()}: {result.fee} {result.unit}"
-    )
+    if FeeCache.is_new_value(result, update_cache=True):
+        LoggerFactory.get_logger().info(
+            f"Fee for {result.blockchain} at {result.timestamp.isoformat()}: {result.fee} {result.unit}"
+        )
 
 
 def log_exception(exception: Exception) -> None:
